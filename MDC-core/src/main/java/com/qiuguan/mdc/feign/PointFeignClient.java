5e7b3eba0febfea0451d0609650cb8a569275983
qiuguan <18368116334@163.com>
2023-07-03 23:58:01 +0800

    测试feign调用MDC的日志打印，目前测试没有通过，后面再看

---
+++ MDC-core/src/main/java/com/qiuguan/mdc/feign/PointFeignClient.java
@@ -0,0 +1,5 @@
+package com.qiuguan.mdc.feign;/**
+ * @author qiuguan
+ * @date 2023/07/03 23:34:26  星期一
+ */   public class FeignClient {
+}
---
+++ MDC-feign/src/main/java/com/qiuguan/mdc/feign/FeignApplication.java
@@ -0,0 +1,5 @@
+package com.qiuguan.mdc.feign.controller;/**
+ * @author qiuguan
+ * @date 2023/07/03 23:31:52  星期一
+ */   public class FeignApplication {
+}
