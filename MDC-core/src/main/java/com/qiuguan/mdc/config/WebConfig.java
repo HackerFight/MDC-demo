7b8d1167a011f144a14ebe550e008f8ae3527c96
qiuguan <18368116334@163.com>
2023-11-29 23:49:04 +0800

    fix类移动导致的错误

--- MDC-core/src/main/java/com/qiuguan/mdc/config/WebConfig.java
+++ MDC-core/src/main/java/com/qiuguan/mdc/config/WebConfig.java
@@ -1,18 +1,14 @@
 package com.qiuguan.mdc.config;
 
-import com.qiuguan.mdc.interceptors.HttpRequestInterceptor;
 import com.qiuguan.mdc.interceptors.MdcLogInterceptor;
 import com.qiuguan.mdc.threadpool.ThreadPoolExecutorMdcWrapper;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Collections;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -48,22 +44,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public ThreadPoolExecutor threadPoolExecutor(){
         return new ThreadPoolExecutorMdcWrapper(5, 10,30L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(1024));
-    }
-
-    //==========================http================================//
-
-    @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setInterceptors(Collections.singletonList(httpRequestInterceptor()));
-
-        return restTemplate;
-    }
-
-
-    @Bean
-    public ClientHttpRequestInterceptor httpRequestInterceptor(){
-        return new HttpRequestInterceptor();
     }
 
 }
