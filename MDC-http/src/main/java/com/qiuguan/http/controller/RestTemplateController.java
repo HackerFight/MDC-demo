654a832d8a32b35b387f79aaeb75117d1ab7b3aa
qiuguan <18368116334@163.com>
2023-11-29 08:35:38 +0800

    HTTP调用初步提交

---
+++ MDC-http/src/main/java/com/qiuguan/http/controller/RestTemplateController.java
@@ -0,0 +1,12 @@
+package com.qiuguan.http.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author qiuguan
+ * @date 2023/11/28 21:57:38  星期二
+ */
+@RestController
+public class RestTemplateController {
+
+}
--- MDC-http/src/main/java/com/qiuguan/http/controller/WebClientController.java
+++ MDC-http/src/main/java/com/qiuguan/http/controller/WebClientController.java
@@ -4,8 +4,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author qiuguan
- * @date 2023/11/28 21:57:38  星期二
+ * @date 2023/11/28 23:26:59  星期二
  */
 @RestController
-public class HttpController {
+public class WebClientController {
+
 }
